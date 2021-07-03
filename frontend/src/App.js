95ec6d0af5ecc0361af1ee22e24f89b824b5416f
katya <katiamotolianec@gmail.com>
2021-07-04 00:06:11 +0300

    add axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      const [film, setFilm] = useState( [])
+
+      useEffect( () => {
+            axios({
+                  method: "GET",
+                  url: 'http://127.0.0.1:8000/api/top-films/',
+
+            }).then(response => {
+                  setFilm(response.data)
+            })
+      }, [])
+      return (
+          <div>
+                <h1>Hhhhhhh</h1>
+                <div>
+                      {film.map((f) => {
+                            return(
+                                <ul>
+                                      <li>{f.nameRu}</li>
+                                </ul>
+                            )
+                      })}
+
+                </div>
+          </div>
+      );
 }
+/*class App extends React.Component {
+
+      render() {
+            this.state = []
+            fetch('http://127.0.0.1:8000/api/top-films/')
+                .then(response => response.json())
+      return (
+          <div>
+                {this.state.map((info) => {
+                      return (
+                          <div>
+                                <h1>{info.nameRu}</h1>
+                                <h2>{info.age}</h2>
+                          </div>
+                      );
+                })}
+          </div>
+      );
+      }
+}*/
 
 export default App;
