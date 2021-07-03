b6697f7654c9a50169133b8764b70d8385900383
Артём Таболич <tabolich.artyom@mail.ru>
2021-07-03 19:35:52 +0300

    change to sqlite

--- kinopoiskclone/settings.py
+++ kinopoiskclone/settings.py
@@ -76,17 +76,22 @@ WSGI_APPLICATION = 'kinopoiskclone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'KinopoiskClone',
+#         'USER': 'postgres',
+#         'PASSWORD': '123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'KinopoiskClone',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'KinopoiskClone.db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
