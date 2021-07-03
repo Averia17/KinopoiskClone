f9c7ccb70381c51ff145f54ce0c49cb18c38e05d
katya <katiamotolianec@gmail.com>
2021-07-03 19:59:53 +0300

    add django-cors-headers

--- kinopoiskclone/settings.py
+++ kinopoiskclone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'backend',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'kinopoiskclone.urls'
@@ -139,3 +141,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    "https://localhost:3000",
+]
