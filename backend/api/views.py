e13c18916672902e99148f374ad48be906997fc7
Артём Таболич <tabolich.artyom@mail.ru>
2021-08-11 18:31:48 +0300

    fix duplicates misstake

--- backend/api/views.py
+++ backend/api/views.py
@@ -18,39 +18,24 @@ class FilmsViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = Film.objects.filter(type='FILM').\
             order_by('-rating').values_list('id', 'image', 'name', 'year', 'genres__title', named=True)
-        # for item in queryset:
-        # for item in queryset:
-        #     print(item)
-        #     print(getattr(item.genres.first(), 'title', None))
-        serializer = FilmListSerpySerializer(queryset, many=True)
-        #print(serializer.data)
+        # maybe i will remove this and make normal queryset
+        existing_slots = []
+        new_queryset = []
+        for placeholder in queryset:
+            if placeholder.id in existing_slots:
+                continue
+            else:
+                existing_slots.append(placeholder.id)
+                new_queryset.append(placeholder)
+
+        serializer = FilmListSerpySerializer(new_queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         film = Film.objects.get(pk=pk)
         serializer = FilmSerializer(film)
         return Response(serializer.data)
-    # @staticmethod
-    # def get_queryset():
-    #     # data = cache.get('films')
-    #     # if data is None:
-    #     #     films = Film.objects.filter(type='FILM').order_by('-rating')
-    #     #     serializer = FilmListSerializer(films)
-    #     #     data = serializer.data
-    #     #     cache.set('films', films, 86400)
-    #     # print(data)
-    #     # check_if_empty_films()
-    #     # Полная дичь но слайсы нельзя делать...
-    #     # n = 0
-    #     # films = []
-    #     # for film in Film.objects.filter(type='FILM').order_by('-rating'):
-    #     #     if n == 50:
-    #     #         return films
-    #     #     films.append(film)
-    #     #
-    #     #     n += 1
-    #
-    #     return Film.objects.filter(type='FILM').order_by('-rating') # Film.objects.filter(type='FILM').order_by('-rating')
+
 
 
 class SerialsViewSet(viewsets.ModelViewSet):
