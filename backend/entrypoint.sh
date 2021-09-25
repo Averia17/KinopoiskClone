39e5cd68e99c47c8373977c72a5a8d7915e3657f
Артём Таболич <tabolich.artyom@mail.ru>
2021-09-25 09:37:53 +0300

    fix some bugs with static

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-    
-    while ! nc -z $DB_HOST $DB_PORT; do
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
-    
+
     echo "PostgreSQL started"
 fi
 
+# Uncomment below to flush db e.g. after running tests
+# Just make sure you really mean it
+# python manage.py flush --no-input
+
+python manage.py makemigrations
+
+python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
 
 exec "$@"
