53d913fffe5723257ed3cb1daa6fd9e8363c959d
Артём Таболич <tabolich.artyom@mail.ru>
2021-09-25 08:16:13 +0300

    update docker

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -13,6 +13,5 @@ fi
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py migrate --database=main --noinput
 
 exec "$@"
